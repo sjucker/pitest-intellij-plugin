41fb25fc436ba7e53d80a12c8250464770b8a361
GitHub Action <action@github.com>
2021-09-08 19:00:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sjucker/pitestintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sjucker.pitestintellijplugin.services
+
+import com.github.sjucker.pitestintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
